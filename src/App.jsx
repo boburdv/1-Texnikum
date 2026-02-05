a926a9a27000161dc7295e0a449cc9c4dceee42c
boburdv <boburdv@gmail.com>
2026-02-05 13:35:17 -0600

    App routing update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
-import CategoryPage from "./components/Category";
-import AdCategory from "./components/AdCategory";
+import About from "./pages/About";
 import AdminPanel from "./pages/Admin";
-import AdDetail from "./components/AdDetail";
 import ChatPage from "./pages/AllChats";
 import Auth from "./pages/Auth";
-import About from "./pages/About";
-import { Toaster } from "react-hot-toast";
+
+import CategoryPage from "./components/Category";
+import AdCategory from "./components/AdCategory";
+import AdDetail from "./components/AdDetail";
+
+import { useAuthStore } from "./store/authStore";
 
 function AppWrapper() {
   const { pathname } = useLocation();
+
+  const init = useAuthStore((s) => s.init);
+
+  useEffect(() => {
+    init();
+  }, [init]);
 
   const showLayout = pathname === "/" || pathname === "/about";
 
